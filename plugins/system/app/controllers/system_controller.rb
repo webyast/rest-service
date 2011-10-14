2e9535ff4141ed9d4a343be84b942d08ff1fd536
vlewin <vlewin@suse.de>
2011-10-14 12:32:43 +0200

    - fixed reboot/shutdown issue on openSUSE 12.1-beta1

--- plugins/system/app/controllers/system_controller.rb
+++ plugins/system/app/controllers/system_controller.rb
@@ -1,91 +1,90 @@
 #--
 # Copyright (c) 2009-2010 Novell, Inc.
-# 
+#
 # All Rights Reserved.
-# 
+#
 # This program is free software; you can redistribute it and/or modify it
 # under the terms of version 2 of the GNU General Public License
 # as published by the Free Software Foundation.
-# 
+#
 # This program is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
 # GNU General Public License for more details.
-# 
+#
 # You should have received a copy of the GNU General Public License
 # along with this program; if not, contact Novell, Inc.
-# 
+#
 # To contact Novell about this file by physical or electronic mail,
 # you may find current contact information at www.novell.com
 #++
 
 
 class SystemController < ApplicationController
+  before_filter :login_required
 
-    before_filter :login_required
+  def show
+    @actions = System.instance.actions
 
-    def show
-   	@actions = System.instance.actions
-
-	respond_to do |format|
-	    format.xml  { render :xml => @actions.to_xml(:root => :actions), :location => "none" }
-	    format.json { render :json => @actions.to_json, :location => "none" }
-	end
+    respond_to do |format|
+      format.xml  { render :xml => @actions.to_xml(:root => :actions), :location => "none" }
+      format.json { render :json => @actions.to_json, :location => "none" }
     end
-   
-    def update
-	root = params[:system]
-	if root == nil || root == {} 
-	  render ErrorResult.error(404, 2, "format error - missing actions") and return
-	end
-	
-	@system = System.instance
+  end
 
-	do_reboot = false
-	do_shutdown = false
-
-	# do the action
-	root.each do |k, v|
-	    if v.nil? or !v.respond_to?('has_key?') or !v.has_key? 'active'
-		render ErrorResult.error(404, 2, "format error - missing requested status") and return
-	    end
-
-	    if v['active'] != true and v['active'] != false
-		render ErrorResult.error(404, 2, "format error - non-boolean active parameter") and return
-	    end
-
-	    # unknown action requested
-	    if !@system.actions.has_key? k.to_sym
-		render ErrorResult.error(404, 2, "format error - unknown action requested") and return
-	    end
-
-	    case k
-		when 'reboot'
-		    permission_check( 'org.opensuse.yast.system.power-management.reboot')
-
-		    if v['active'] == true and @system.actions[k.to_sym][:active] == false
-			do_reboot = true
-		    end
-		when 'shutdown'
-		    permission_check( 'org.opensuse.yast.system.power-management.shutdown')
-
-		    if v['active'] == true and @system.actions[k.to_sym][:active] == false
-			do_shutdown = true
-		    end
-		else
-		    render ErrorResult.error(404, 2, "internal error - unknown action requested") and return
-	    end
-	end
-
-	if do_reboot then @system.reboot end
-	if do_shutdown then @system.shutdown end
-
-	show
+  def update
+    root = params[:system]
+    if root == nil || root == {}
+      render ErrorResult.error(404, 2, "format error - missing actions") and return
     end
 
-    # See update
-    def create
-	update
+    @system = System.instance
+
+    do_reboot = false
+    do_shutdown = false
+
+    # do the action
+    root.each do |k, v|
+      if v.nil? or !v.respond_to?('has_key?') or !v.has_key? 'active'
+        render ErrorResult.error(404, 2, "format error - missing requested status") and return
+      end
+
+      if v['active'] != true and v['active'] != false
+        render ErrorResult.error(404, 2, "format error - non-boolean active parameter") and return
+      end
+
+      # unknown action requested
+      if !@system.actions.has_key? k.to_sym
+        render ErrorResult.error(404, 2, "format error - unknown action requested") and return
+      end
+
+      case k
+      when 'reboot'
+        permission_check( 'org.opensuse.yast.system.power-management.reboot')
+
+        if v['active'] == true and @system.actions[k.to_sym][:active] == false
+          do_reboot = true
+        end
+      when 'shutdown'
+        permission_check( 'org.opensuse.yast.system.power-management.shutdown')
+
+        if v['active'] == true and @system.actions[k.to_sym][:active] == false
+          do_shutdown = true
+        end
+      else
+        render ErrorResult.error(404, 2, "internal error - unknown action requested") and return
+      end
     end
 
+    if do_reboot then @system.reboot end
+    if do_shutdown then @system.shutdown end
+
+    show
+  end
+
+  # See update
+  def create
+   update
+  end
 end
+
--- plugins/system/app/models/system.rb
+++ plugins/system/app/models/system.rb
@@ -1,20 +1,20 @@
 #--
 # Copyright (c) 2009-2010 Novell, Inc.
-# 
+#
 # All Rights Reserved.
-# 
+#
 # This program is free software; you can redistribute it and/or modify it
 # under the terms of version 2 of the GNU General Public License
 # as published by the Free Software Foundation.
-# 
+#
 # This program is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
 # GNU General Public License for more details.
-# 
+#
 # You should have received a copy of the GNU General Public License
 # along with this program; if not, contact Novell, Inc.
-# 
+#
 # To contact Novell about this file by physical or electronic mail,
 # you may find current contact information at www.novell.com
 #++
@@ -25,79 +25,76 @@ require 'singleton'
 
 
 class System
-    attr_reader :actions
+  attr_reader :actions
+  include Singleton
 
-    include Singleton
+  def initialize
+    @actions = {:reboot => {:active => false}, :shutdown => {:active => false} }
+  end
 
-    def initialize
-	@actions = {:reboot => {:active => false}, :shutdown => {:active => false} }
+  def reboot
+    if consolekit_power_management(:reboot)
+      @actions[:reboot][:active] = true
     end
+  end
 
-    def reboot
-	if hal_power_management(:reboot)
-	    @actions[:reboot][:active] = true
-	end
+  def shutdown
+    if consolekit_power_management(:shutdown)
+      @actions[:shutdown][:active] = true
     end
+  end
 
-    def shutdown
-	if hal_power_management(:shutdown)
-	    @actions[:shutdown][:active] = true
-	end
+
+  private
+
+  def consolekit_power_management(action, params = {})
+    return false unless action == :reboot or action == :shutdown
+
+    begin
+      # connect to the system bus
+      # Make a fresh connection, to be able to reboot
+      # after DBus is restarted, bnc#582759
+      system_bus = DBus::SystemBus.send :new # RORSCAN_ITL
+      consolekit = system_bus.service('org.freedesktop.ConsoleKit') # RORSCAN_ITL
+      system = consolekit.object('/org/freedesktop/ConsoleKit/Manager')
+      system.introspect
+      system.default_iface = 'org.freedesktop.ConsoleKit.Manager'
+
+      case action
+
+      when :reboot
+        if ENV['RAILS_ENV'] == 'production'
+          Rails.logger.info "Rebooting the computer..."
+          return system.Restart
+        else
+          Rails.logger.warn "Skipping reboot in #{ENV['RAILS_ENV']} mode"
+         return true
+        end
+
+      when :shutdown
+        if ENV['RAILS_ENV'] == 'production'
+          Rails.logger.info "Shutting down the computer..."
+          return system.Stop
+        else
+          Rails.logger.warn "Skipping shutdown in #{ENV['RAILS_ENV']} mode"
+          return true
+        end
+
+      else
+        Rails.logger.error "Unsupported ConsoleKit command: #{action}"
+      end
+
+      # handle DBus errors
+      rescue DBus::Error => dbe
+        Rails.logger.error "DBus error: #{dbe.dbus_message.error_name}"
+        return false
+
+      # handle generic errors
+      rescue Exception => e
+        Rails.logger.error "Caught exception: #{e.message}"
+        return false
+
     end
-
-
-# private methods
-    private
-
-    def hal_power_management(action, params = {})
-
-	return false unless action == :reboot or action == :shutdown
-
-	begin
-	    # connect to the system bus
-            # Make a fresh connection, to be able to reboot
-            # after DBus is restarted, bnc#582759
-	    system_bus = DBus::SystemBus.send :new # RORSCAN_ITL
-
-	    # get the HAL service
-	    hal_service = system_bus.service('org.freedesktop.Hal') # RORSCAN_ITL
-
-	    computer = hal_service.object('/org/freedesktop/Hal/devices/computer')
-	    computer.introspect
-	    computer.default_iface = 'org.freedesktop.Hal.Device.SystemPowerManagement' # RORSCAN_ITL
-
-	    case action
-
-		when :reboot
-		    if ENV['RAILS_ENV'] == 'production'
-			Rails.logger.info 'Rebooting the computer...'
-			return computer.Reboot.zero?
-		    else
-			Rails.logger.warn "Skipping reboot in #{ENV['RAILS_ENV']} mode"
-			return true
-		    end
-		when :shutdown
-		    if ENV['RAILS_ENV'] == 'production'
-			Rails.logger.info 'Shutting down the computer...'
-			return computer.Shutdown.zero?
-		    else
-			Rails.logger.warn "Skipping shutdown in #{ENV['RAILS_ENV']} mode"
-			return true
-		    end
-		else
-		    Rails.logger.error "Unsupported HAL command: #{action}"
-	    end
-
-	# handle DBus errors
-	rescue DBus::Error => dbe
-	    Rails.logger.error "DBus error: #{dbe.dbus_message.error_name}"
-	    return false
-	# handle generic errors
-	rescue Exception => e
-	    Rails.logger.error "Caught exception: #{e.message}"
-	    return false
-	end
-    end
-
-
+  end
 end
+
